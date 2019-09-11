8996617cf22831670dc8e0fa1a65e0738629b4c2
itaysel <itaymailsela@gmail.com>
2019-09-11 13:03:39 +0300

    Changes to pom.xml Removed logger name

--- src/main/Slf4jLogger.java
+++ src/main/Slf4jLogger.java
@@ -2,13 +2,11 @@ import org.slf4j.LoggerFactory;
 
 class Slf4jLogger {
 
-    private static final String LOGGER_NAME = "UrlScannerLogger";
-
     private static org.slf4j.Logger log;
 
     static org.slf4j.Logger getLogger() {
         if (log == null) {
-            log = LoggerFactory.getLogger(LOGGER_NAME);
+            log = LoggerFactory.getLogger("");
         }
         return log;
     }
